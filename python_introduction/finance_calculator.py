0351f2ae4fb183feb9115b687dced8c13b0cfcd7
MIRACLE773 <mjschool48@gmail.com>
2025-06-02 01:57:30 -0700

    Add weather advice script in control-flow folder

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -8,3 +8,4 @@ projected_savings = annual_savings + (annual_savings * 0.05)
 print(f"Your monthly savings are ${monthly_savings}.")
 print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
 
+
