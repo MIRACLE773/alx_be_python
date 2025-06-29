8020a3669a68b75883514b9399d644805c4a3d4a
MIRACLE773 <mjschool48@gmail.com>
2025-06-28 22:42:36 -0700

    add alx_book_store.sql with database shema

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,27 +1,5 @@
-from datetime import datetime
-import pytz
+import json
 
-def show_time(city):
-    # Dictionary of city to timezone mapping
-    timezones = {
-        "houston": "America/Chicago",
-        "new york": "America/New_York",
-        "london": "Europe/London",
-        "lagos": "Africa/Lagos",
-        "nairobi": "Africa/Nairobi",
-        "tokyo": "Asia/Tokyo",
-        "dubai": "Asia/Dubai"
-    }
+data {""}
 
-    city = city.lower()
 
-    if city in timezones:
-        tz = pytz.timezone(timezones[city])
-        city_time = datetime.now(tz)
-        print(f"The time in {city.title()} is {city_time.strftime('%I:%M:%S %p on %A, %B %d, %Y')}")
-    else:
-        print("Sorry, that city is not in our list.")
-
-# Ask user for input
-user_city = input("Enter a city (e.g., Houston, London, Lagos): ")
-show_time(user_city)
--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,11 +1,14 @@
-class Calculator:
-    calculation_type = "Arithmetic Operations"
-
-    @staticmethod
-    def add(a, b):
-        return a + b
-
-    @classmethod
-    def multiply(cls, a, b):
-        print(f"Calculation type: {cls.calculation_type}")
-        return a * b
+class book:
+    def __init__(self,title, author, year):
+        self.title = title 
+        self.author = author
+        self.year = year
+        
+    def __del__(self):
+        return f"deleting {self.title}"
+    
+    def __str__(self):
+        return f"{self.title} by {self.author} published in {self.year}"
+    
+    def __repr__(self):
+        return f"book:({self.title}) author:({self.author}) year({self.year})"
